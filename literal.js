ad181d742934e93b12d8ca47fd1212459495a4ad
smesher <betelgeusems@gmail.com>
2022-06-16 10:25:59 -0700

    Add files via upload

---
+++ literal.js
@@ -0,0 +1,12 @@
+const product = {
+    name: "shiny shoes",
+    brand: "gucci",
+    price: 100,
+    color: "black",
+    soldOut: false,
+    sale: true
+};
+
+console.log(product);
+
+console.log('Name: ' + product['name']);
